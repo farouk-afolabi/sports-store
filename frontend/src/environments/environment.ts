372c3c726b7b47ec1b8333c9244c7932b77b0fdb
Farouk Afolabi <farouk.afolabi@yahoo.com>
2025-06-25 14:08:14 -0400

    changes

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -2,10 +2,12 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3500'
+  apiUrl: 'https://sports-store-m8wv.onrender.com'
 };
+
 
 /*
  * For easier debugging in development mode, you can import the following file
