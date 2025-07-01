a3c924f2b9abe19475c370f69282e5fc47b94e4e
Farouk Afolabi <farouk.afolabi@yahoo.com>
2025-07-01 16:48:15 -0400

    adjusted port number from 3500 to 3000

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,8 @@ app.use(auth);
 app.use("/api", router);
 
 // Using Render-assigned port
-const PORT = process.env.PORT || 3500;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+  
